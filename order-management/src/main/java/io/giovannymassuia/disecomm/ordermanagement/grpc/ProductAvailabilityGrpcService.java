dd24fd7feee8e2e8ca415efa7623a3ec478ef785
Giovanny Massuia <giovannycmassuia@gmail.com>
2024-04-14 09:57:22 -0700

    test custom span for tracing

--- order-management/src/main/java/io/giovannymassuia/disecomm/ordermanagement/grpc/ProductAvailabilityGrpcService.java
+++ order-management/src/main/java/io/giovannymassuia/disecomm/ordermanagement/grpc/ProductAvailabilityGrpcService.java
@@ -6,14 +6,23 @@ import io.giovannymassuia.disecomm.protobuf.order.productavailability.ProductAva
 import io.grpc.stub.StreamObserver;
 import java.time.LocalDate;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class ProductAvailabilityGrpcService extends
     ProductAvailabilityServiceGrpc.ProductAvailabilityServiceImplBase {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(
+        ProductAvailabilityGrpcService.class);
+
+
     @Override
     public void checkProductAvailability(CheckProductAvailabilityRequest request,
         StreamObserver<CheckProductAvailabilityResponse> responseObserver) {
+
+        LOGGER.info("Checking product availability for product with id: {}",
+            request.getProductId());
 
         responseObserver.onNext(CheckProductAvailabilityResponse.newBuilder()
                                     .setInStock(true)
@@ -21,6 +30,10 @@ public class ProductAvailabilityGrpcService extends
                                         LocalDate.now().toString())
                                     .setQuantityAvailable(5)
                                     .build());
+
+        LOGGER.info("Done checking product availability for product with id: {}",
+            request.getProductId());
+
         responseObserver.onCompleted();
 
     }
--- product-catalog/src/main/java/io/giovannymassuia/disecomm/productcatalog/grpc/ProductGrpcService.java
+++ product-catalog/src/main/java/io/giovannymassuia/disecomm/productcatalog/grpc/ProductGrpcService.java
@@ -1,6 +1,5 @@
 package io.giovannymassuia.disecomm.productcatalog.grpc;
 
-import io.giovannymassuia.disecomm.productcatalog.model.ProductAnalytics;
 import io.giovannymassuia.disecomm.productcatalog.model.ProductAnalyticsRepository;
 import io.giovannymassuia.disecomm.protobuf.order.productavailability.CheckProductAvailabilityRequest;
 import io.giovannymassuia.disecomm.protobuf.order.productavailability.ProductAvailabilityServiceGrpc;
@@ -11,12 +10,19 @@ import io.giovannymassuia.disecomm.protobuf.product.ListProductsResponse;
 import io.giovannymassuia.disecomm.protobuf.product.Product;
 import io.giovannymassuia.disecomm.protobuf.product.ProductServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @GrpcService
 public class ProductGrpcService extends ProductServiceGrpc.ProductServiceImplBase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductGrpcService.class);
 
     @GrpcClient("product-availability-service")
     private ProductAvailabilityServiceGrpc.ProductAvailabilityServiceBlockingStub productAvailabilityService;
@@ -24,21 +30,31 @@ public class ProductGrpcService extends ProductServiceGrpc.ProductServiceImplBas
     @Autowired
     private ProductAnalyticsRepository productAnalyticsRepository;
 
+    private final Tracer tracer;
+
+    public ProductGrpcService(OpenTelemetry openTelemetry) {
+        tracer = openTelemetry.getTracer(ProductGrpcService.class.getName());
+    }
+
     @Override
     public void getProduct(GetProductRequest request,
         StreamObserver<GetProductResponse> responseObserver) {
 
-        // compute analytics
-        productAnalyticsRepository.findById(request.getId())
-            .ifPresentOrElse(
-                productAnalytics -> {
-                    productAnalyticsRepository.save(
-                        productAnalytics.withCount(productAnalytics.count() + 1));
-                },
-                () -> {
-                    productAnalyticsRepository.save(new ProductAnalytics(request.getId(), 1));
-                }
-            );
+        Span span = tracer.spanBuilder("getProductStart").startSpan();
+
+        LOGGER.info("Get product with id: {}", request.getId());
+
+//        // compute analytics
+//        productAnalyticsRepository.findById(request.getId())
+//            .ifPresentOrElse(
+//                productAnalytics -> {
+//                    productAnalyticsRepository.save(
+//                        productAnalytics.withCount(productAnalytics.count() + 1));
+//                },
+//                () -> {
+//                    productAnalyticsRepository.save(new ProductAnalytics(request.getId(), 1));
+//                }
+//            );
 
         responseObserver.onNext(GetProductResponse.newBuilder()
                                     .setProduct(Product.newBuilder()
@@ -52,6 +68,10 @@ public class ProductGrpcService extends ProductServiceGrpc.ProductServiceImplBas
                                                                             .build()).getInStock()
                                                     ).build())
                                     .build());
+
+        LOGGER.info("Product get successfully");
+        span.end();
+
         responseObserver.onCompleted();
     }
 
