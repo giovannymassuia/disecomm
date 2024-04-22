fa3089dfb07345ee7831bf3e071adb730310a24c
Giovanny Massuia <giovannycmassuia@gmail.com>
2024-04-22 06:46:52 -0700

    inventory service - add inventory table

---
+++ inventory-management/src/main/resources/db/migration/V20240421.1500___createUpdatedAtFunction.sql
@@ -0,0 +1,10 @@
+create or replace function update_updated_at()
+    returns trigger as
+$$
+begin
+    new.updated_at
+        = now();
+    return new;
+end;
+$$
+    language plpgsql;
---
+++ inventory-management/src/main/resources/db/migration/V20240421.1600__inventory_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE inventory
+(
+    sku        VARCHAR(50) PRIMARY KEY,
+    on_hand    INT       NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER update_inventory_updated_at
+    BEFORE UPDATE
+    ON inventory
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at();
