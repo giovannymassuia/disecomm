8cd70de6e791e1833a1a3eae99c30ccbaf83bd43
Giovanny Massuia <giovannycmassuia@gmail.com>
2024-10-21 07:04:00 -0700

    create k6 scripts for local and docker

--- local-dev/k6/run_docker.sh
+++ local-dev/k6/run_docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# network shared-disecomm-network
+
+# running local ref
+# k6 run --out experimental-prometheus-rw main.js
+
 # set container name to k6-load-test
 docker run -i \
+  --rm \
   -e DOCKER=true \
-  -e K6_PROMETHEUS_RW_SERVER_URL="http://host.docker.internal:9090/api/v1/write" \
-  -e K6_PROMETHEUS_RW_TREND_STATS="p(95),p(99),min,max" \
+  -e K6_PROMETHEUS_RW_SERVER_URL="http://prometheus:9090/api/v1/write" \
+  -e K6_PROMETHEUS_RW_TREND_STATS="p(95),p(99),min,max,sum,avg,med" \
   -e K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=true \
+  --network shared-disecomm-network \
   grafana/k6 run - < main.js
-
-#export K6_PROMETHEUS_RW_SERVER_URL="http://host.docker.internal:9090/api/v1/write"
-#export K6_PROMETHEUS_RW_TREND_STATS="p(95),p(99),min,max"
-#export K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=true
